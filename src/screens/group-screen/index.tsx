197c7dd1769c02c5d4a3a2beff863622ea622a96
unknown <xlongclaw@gmail.com>
2024-04-10 20:52:39 +0530

    chore(ui): split navbar into react components

--- src/screens/group-screen/index.tsx
+++ src/screens/group-screen/index.tsx
@@ -4,8 +4,21 @@ export default function GroupScreen() {
   return (
     <div>
       <NavigationBar />
-      <div>
-        <img className="w-100" src="group-bg.png" alt="" />
+      <div
+        className="text-white d-flex flex-column justify-content-end"
+        style={{
+          backgroundImage: "url(group-bg.png)",
+          height: 440,
+          paddingLeft: 190,
+          paddingBottom: 70,
+        }}
+      >
+        <p style={{ lineHeight: "50px" }} className="fs-1 fw-bold">
+          Computer Engineering
+        </p>
+        <p style={{ lineHeight: 0 }} className="fs-5 fw-normal">
+          142,765 Computer Engineers follow this
+        </p>
       </div>
     </div>
   );
