b3f180df43fac4666fa12f4bd69e89575f712e4e
unknown <xlongclaw@gmail.com>
2024-04-10 18:55:12 +0530

    init: initialize a react project with vite and ts, install packages bootstrap5.3.3 and sass

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import GroupScreen from "@screens/group-screen";
+
+export default function App(): React.JSX.Element {
+  return (
+    <main>
+      <GroupScreen />
+    </main>
+  );
+}
